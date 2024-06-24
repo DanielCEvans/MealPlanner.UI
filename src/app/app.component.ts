973778f681931736c85382325125a5d8cb321063
Daniel Evans <daniel.evans@dyedurham.com>
2024-06-24 17:01:28 +1000

    feat: add get request to get all meals on application start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'meal-planner-ui';
+
+  constructor(private http: HttpClient){};
+
+  ngOnInit(){
+    const backendUrl = 'http://localhost:5157/api/meals';
+    this.http.get(backendUrl).subscribe(response => {
+      console.log(response);
+    })
+  }
 }
