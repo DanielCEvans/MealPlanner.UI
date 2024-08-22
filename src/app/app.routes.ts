4f94a33b7988bda8b2888bf41c52f472fc990297
Daniel Evans <daniel.evans@dyedurham.com>
2024-08-22 17:21:03 +1000

    feat: import components into app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AddMealComponent } from './add-meal/add-meal.component';
+import { AddIngredientComponent } from './add-ingredient/add-ingredient.component';
+import { FrigeComponent } from './frige/frige.component';
+import { SelectMealsComponent } from './select-meals/select-meals.component';
 
 export const routes: Routes = [];
