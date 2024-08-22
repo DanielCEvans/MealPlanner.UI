a00d9fc8e718d136ce17c7605e468afdf231d48b
Daniel Evans <daniel.evans@dyedurham.com>
2024-08-22 17:24:18 +1000

    feat: define routes in routes array

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+import { SelectMealsComponent } from './select-meals/select-meals.component';
+import { FrigeComponent } from './frige/frige.component';
 import { AddMealComponent } from './add-meal/add-meal.component';
 import { AddIngredientComponent } from './add-ingredient/add-ingredient.component';
-import { FrigeComponent } from './frige/frige.component';
-import { SelectMealsComponent } from './select-meals/select-meals.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'select-meals', component: SelectMealsComponent },
+  { path: 'your-fridge', component: FrigeComponent },
+  { path: 'add-meals', component: AddMealComponent },
+  { path: 'add-ingredient', component: AddIngredientComponent },
+];
